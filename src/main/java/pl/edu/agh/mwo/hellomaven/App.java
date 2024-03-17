70423da56ba6ea78f0e486c2528f04b210e22cab
rudy <grzerudy@gmail.com>
2024-03-17 14:17:49 +0100

    Do poprawy

--- src/main/java/pl/edu/agh/mwo/hellomaven/App.java
+++ src/main/java/pl/edu/agh/mwo/hellomaven/App.java
@@ -1,5 +1,10 @@
 package pl.edu.agh.mwo.hellomaven;
 
+import com.indvd00m.ascii.render.Render;
+import com.indvd00m.ascii.render.ICanvas;
+import com.indvd00m.ascii.render.api.IContextBuilder;
+import com.indvd00m.ascii.render.api.IRender;
+import com.indvd00m.ascii.render.elements.PseudoText;
 /**
  * Simple main application
  */
@@ -8,7 +13,20 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
-    
+
+        IRender render = new Render();
+        IContextBuilder builder = render.newBuilder();
+            builder.width(120).
+
+        height(20);
+            builder.element(new
+
+        PseudoText("PseudoText"));
+        ICanvas canvas = render.render(builder.build());
+        String s = canvas.getText();
+            System.out.println(s);
+}
+
     public int getRandomNumber() {
         // chosen by fair dice roll.
         // guaranteed to be random
